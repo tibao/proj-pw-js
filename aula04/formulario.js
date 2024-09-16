906f1d1855184efebe90b1d0411adb3e0e20151a
Vinicius Tibo <viniciustiboff@gmail.com>
2024-09-16 18:33:12 -0300

    commit

--- aula04/formulario.js
+++ aula04/formulario.js
@@ -1,34 +1,32 @@
 let proximold = 1;
 
-function Adicionar() {
+function adicionarLinha() {
   const nome = document.getElementById("nome").value;
   const idade = document.getElementById("idade").value;
   const admissao = document.getElementById("admissao").value;
   const demissao = document.getElementById("demissao").value;
 
-  if (nome === "" || idade === "" || admissao === "")
+  if (nome === '' || idade === '' || admissao === ''){
     alert("Preencha os valores do formulário!");
+  } else {
+    const tabela = document.getElementById("tabeladados").getElementsByTagName("tbody")[0];
 
-  const tabela = document
-    .getElementById("tabeladados")
-    .getElementsByTagName("tbody")[0];
+    const novaLinha = tabela.insertRow();
 
-  const novaLinha = tabela.insertRow;
+    const celId = novaLinha.insertCell(0);
+    const celNome = novaLinha.insertCell(1);
+    const celIdade = novaLinha.insertCell(2);
+    const celAdmissao = novaLinha.insertCell(3);
+    const celDemissao = novaLinha.insertCell(4);
 
-  const celId = novaLinha.inserCell(0);
-  const celNome = novaLinha.insertCell(1);
-  const celIdade = novaLinha.inserCell(2);
-  const celAdmissao = novaLinha.inserCell(3);
-  const celDemissao = novaLinha.insertCell(4);
+    celId.innerHTML = proximold;
+    celNome.innerHTML = nome;
+    celIdade.innerHTML = idade;
+    celAdmissao.innerHTML = admissao;
+    celDemissao.innerHTML = demissao;
 
-  celId.innerHTML = proximold;
-  celNome.innerHTML = nome;
-  celIdade.innerHTML = idade;
-  celAdmissao.innerHTML = admissao;
-  celDemissao.innerHTML = demissao;
+    proximold++;
 
-  proximold++;
-
-  //Limpar Formulario
-  document.getElementById("Linha Formulário").requestFullscreen();
+    document.getElementById("linhaFormulario").reset();
+  }
 }
